226a0f84dd4e969d3cc9a0ce3d555b1a0d253106
Remi Jacquemin <79313646+Yama022@users.noreply.github.com>
2022-10-03 17:05:02 +0200

    :wrench: | input for create new todo + list OK && todoList WIP

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,65 @@
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
 import bgDesktopLight from '../public/images/bg-desktop-light.jpg'
 import moon from '../public/images/icon-moon.svg'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.background}>
-        <Image
-          src={bgDesktopLight}
-          alt="background-desktop"
-        />
 
-        <div className={styles.todo}>
-          <h1>TODO</h1>
-          <div className={styles.moon}>
+  const [todoList, setTodoList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const todoInput = document.getElementById('todoInput').value
+    setTodoList([...todoList, todoInput])
+    document.getElementById('todoInput').value = ''
+  }
+
+  useEffect(() => {
+  }, [todoList])
+  
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.background}>
           <Image
-            src={moon}
-            alt="moon"
+            src={bgDesktopLight}
+            alt="background-desktop"
           />
+
+          <div className={styles.todo}>
+            <h1>TODO</h1>
+
+            <div className={styles.moon}>
+            <Image
+              src={moon}
+              alt="moon"
+            />
+            </div>
+
+            <div className={styles.input}>
+              <form method="get" onSubmit={handleSubmit}>
+              <button className={styles.button}></button>
+              <input
+                type="text"
+                placeholder="Create a new todo..."
+                id='todoInput'
+                name='todoInput'
+              >
+              </input>
+              </form>
+            </div>
           </div>
-        </div>
+      </div>
+
+      <div className={styles.list}>
+        <ul>
+          {todoList.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
     </div>
+    
   )
 }
